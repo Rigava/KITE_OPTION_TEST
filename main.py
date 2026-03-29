b46409f53f315c2efca97708a9b42fec18607085
NetZero <73232007+Rigava@users.noreply.github.com>
2026-03-29 20:26:00 +0530

    Update output to show types of atm_chain and history_df
    Changed the output to display the type of atm_chain and history_df instead of their dtypes.

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ col2.metric("Max Pain", max_pain)
 col3.metric("PCR", round(pcr, 2) if pcr else "-")
 col4.metric("Straddle", round(straddle, 2))
 tbl = pa.Table.from_pandas(atm_chain)
-st.write(atm_chain.dtypes)
+st.write(type(atm_chain))
 
 # st.write(repr(tbl))
 # st.dataframe(tbl)
@@ -178,6 +178,7 @@ st.write(atm_chain.dtypes)
     # st.dataframe(tbl)
 
 with st.expander("📈 Historical Data"):
+    st.write(type(st.session_state.history_df))
     st.write(st.session_state.history_df)
 
 # ---------------- CHARTS ---------------- #
