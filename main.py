f47d5ff50ccf64d4be303515a6ae7dc735cefe8e
NetZero <73232007+Rigava@users.noreply.github.com>
2026-03-29 20:40:48 +0530

    Change dataframe display to type output
    Replaced dataframe display with type output for atm_chain.

--- main.py
+++ main.py
@@ -168,7 +168,7 @@ col2.metric("Max Pain", max_pain)
 col3.metric("PCR", round(pcr, 2) if pcr else "-")
 col4.metric("Straddle", round(straddle, 2))
 tbl = pa.Table.from_pandas(atm_chain)
-st.dataframe(atm_chain)
+st.write(type(atm_chain))
 
 # st.write(repr(tbl))
 # st.dataframe(tbl)
