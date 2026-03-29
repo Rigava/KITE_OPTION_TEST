a92681eb06c201db7e916e5cfdacabb0df9d09a7
NetZero <73232007+Rigava@users.noreply.github.com>
2026-03-29 14:44:02 +0530

    Update display of ATM option chain information
    Replace table schema display with ATM chain info and update dataframe display.

--- main.py
+++ main.py
@@ -168,9 +168,9 @@ col2.metric("Max Pain", max_pain)
 col3.metric("PCR", round(pcr, 2) if pcr else "-")
 col4.metric("Straddle", round(straddle, 2))
 tbl = pa.Table.from_pandas(atm_chain)
-st.write(tbl.schema)
+st.write(atm_chain.info())
 # st.write(repr(tbl))
-st.dataframe(tbl)
+# st.dataframe(tbl)
 
 # ---------------- TABLES ---------------- #
 with st.expander("📌 Current ATM Option Chain"):
